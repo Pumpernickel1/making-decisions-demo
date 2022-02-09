8d78b23be6cb5efcf24c739be323c3f43d070e59
Nick Lauritzen <sirnicklaus@gmail.com>
2022-02-09 13:36:17 -0700

    finished demo

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -1,5 +1,24 @@
 /*
     In this file, we'll be using variables, conditionals,
-    and loops to play out a fight between Jon Snow and
-    Jamie Lannister.
+    and loops to play out a fight between Eowyn and
+    Witch King of Angmar.
 */ 
+let eowynAttack = 50;
+let witchKingAttack = 45;
+
+if(eowynAttack > witchKingAttack) {
+    console.log( "Eowyn is stronger.");
+} else if(witchKingAttack > eowynAttack){
+    console.log( "Witch King is stronger.")
+} else if{
+    console.log("They are the same strength.");
+};
+
+let eowynHealth = 100;
+let eoeynDefense = 0;
+
+if(eowynHealth + eowynDefense <= witchKingAttack) {
+    console.log("She dead.");
+}else{
+    console.log("She alive.");
+};
